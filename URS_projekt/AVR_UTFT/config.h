f5150bd795903423269aba581a526ff4de255419
aKozina <ankz5995@gmail.com>
2022-02-20 01:06:33 +0100

    Uklonjena knjiznica XPT2046_Touchscreen-1.4.0 jer je za Arduino
    Konfigurirani pinovi za display
    Napisana testna main funkcija - žuti kvadrati se popunjavaju i kad dodu do kraja ispočetka

--- URS_projekt/AVR_UTFT/config.h
+++ URS_projekt/AVR_UTFT/config.h
@@ -13,40 +13,40 @@
 
 	
 /* Configure your hardware pins here */
-#define WR_PORT PORTA
-#define WR_PIN	4
-#define WR_PORT_DDR DDRA
+#define WR_PORT PORTC
+#define WR_PIN 6
+#define WR_PORT_DDR DDRC
 
 #define DATA_PORT_LOW PORTD
-#define DATA_PORT_HIGH PORTC
+#define DATA_PORT_HIGH PORTA
 #define DATA_PORT_LOW_DDR DDRD
-#define DATA_PORT_HIGH_DDR DDRC
+#define DATA_PORT_HIGH_DDR DDRA
 
-#define RS_PORT PORTA
-#define RS_PIN 2
-#define RS_PORT_DDR DDRA
+#define RS_PORT PORTC
+#define RS_PIN 7
+#define RS_PORT_DDR DDRC
 
-#define CS_PORT PORTA
-#define CS_PIN 5
-#define CS_PORT_DDR DDRA
+#define CS_PORT PORTC
+#define CS_PIN 0
+#define CS_PORT_DDR DDRC
 
-#define RD_PORT PORTA
-#define RD_PIN 3
-#define RD_PORT_DDR DDRA
+#define RD_PORT PORTB
+#define RD_PIN 0
+#define RD_PORT_DDR DDRB
 
-#define RESET_PORT PORTB
+#define RESET_PORT PORTC
 #define RESET_PIN 1
-#define RESET_PORT_DDR DDRB
+#define RESET_PORT_DDR DDRC
 
 #define BACK_LIGHT_PORT PORTB
-#define BACK_LIGHT_PIN 3
+#define BACK_LIGHT_PIN 1
 #define BACK_LIGHT_DDR DDRB
 
-#define F_CPU 12000000
+#define F_CPU 7372800UL
 
-#define HAVE_STATUS_LED 1	
+#define HAVE_STATUS_LED 0
 #define STATUS_LED_PORT PORTB
-#define STATUS_LED_PIN PORTB0
+#define STATUS_LED_PIN 2
 #define STATUS_LED_DDR DDRB
 	
 	
--- URS_projekt/main.cpp
+++ URS_projekt/main.cpp
@@ -1,11 +1,33 @@
+#define F_CPU 7372800UL
 #include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "AVR_UTFT/UTFT.h"
+#include "AVR_UTFT/color.h"
+#include "AVR_UTFT/had.c"
+#include "AVR_UTFT/DefaultFonts.h"
 
-
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+int main(void) {
+	
+	UTFT display;
+	display.InitLCD(LANDSCAPE);
+	display.setFont(BigFont);
+	display.clrScr();
+	
+	display.setColor(255, 250, 0);
+	
+	uint8_t pos = 0;
+	
+	while (1) {
+		display.fillRect(pos, 0, pos+20, 20);
+		if (pos > 200) {
+			pos = 0;
+			display.clrScr();
+		} else {
+			pos+=20;
+		}
+		delay(100);
+	}
 }
-
