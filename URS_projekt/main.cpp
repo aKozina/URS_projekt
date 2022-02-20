aee6e4d1b236240ab66891b61bca1e782fe09419
aKozina <ankz5995@gmail.com>
2022-02-20 16:26:29 +0100

    Detekcija dodira

--- URS_projekt/main.cpp
+++ URS_projekt/main.cpp
@@ -4,12 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "AVR_UTFT/UTFT.h"
 #include "AVR_UTFT/color.h"
 #include "AVR_UTFT/had.c"
 #include "AVR_UTFT/DefaultFonts.h"
 
+bool isTouched() {
+	// PINB2 low = touched
+	if (~PINB & 0b00001000) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 int main(void) {
+	
+	// PINB2 ulazni - spojeno na pin T_IRQ touch controllera (normally high, low when touching)
+	DDRB &= 0b11110111;
 	
 	UTFT display;
 	display.InitLCD(LANDSCAPE);
@@ -28,6 +41,7 @@ int main(void) {
 		} else {
 			pos+=20;
 		}
+		
 		delay(100);
 	}
 }
