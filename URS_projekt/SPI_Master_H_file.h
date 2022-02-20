bdcf93e6903c2fb02341ffa3b8819f29eb11bf2c
aKozina <ankz5995@gmail.com>
2022-02-20 20:01:05 +0100

    Dodan library za SPI interface i napisane funkcije za dobivanje pozicije dodira na ekranu

---
+++ URS_projekt/SPI_Master_H_file.h
@@ -0,0 +1,34 @@
+/*
+ * SPI_Master_H_file.h
+ *
+ */ 
+
+
+#ifndef SPI_MASTER_H_FILE_H_
+#define SPI_MASTER_H_FILE_H_
+
+#define F_CPU 7372800UL
+#include <avr/io.h>							/* Include AVR std. library file */
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define MOSI 5								/* Define SPI bus pins */
+#define MISO 6
+#define SCK 7
+#define SS 4
+#define SS_Enable PORTB &= ~(1<<SS)			/* Define Slave enable */
+#define SS_Disable PORTB |= (1<<SS)			/* Define Slave disable */
+
+void SPI_Init();							/* SPI initialize function */
+void SPI_Write(char);						/* SPI write data function */
+char SPI_Read();							/* SPI read data function */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SPI_MASTER_H_FILE_H_ */
--- URS_projekt/main.cpp
+++ URS_projekt/main.cpp
@@ -9,6 +9,7 @@
 #include "AVR_UTFT/color.h"
 #include "AVR_UTFT/had.c"
 #include "AVR_UTFT/DefaultFonts.h"
+#include "SPI_Master_H_file.h"
 
 bool isTouched() {
 	// PINB2 low = touched
@@ -19,10 +20,29 @@ bool isTouched() {
 	}
 }
 
+uint16_t getX() {
+	SPI_Write(0b10010000);
+	float x = SPI_Read() / 120.0 * 319;
+	SPI_Write(0b00000000);
+	delay(10);
+	return (uint16_t)x;
+}
+
+uint16_t getY() {
+	SPI_Write(0b11010000);
+	float y = SPI_Read() / 120.0 * 239;
+	SPI_Write(0b00000000);
+	delay(10);
+	return (uint16_t)y;
+}
+
 int main(void) {
 	
 	// PINB2 ulazni - spojeno na pin T_IRQ touch controllera (normally high, low when touching)
 	DDRB &= 0b11110111;
+	
+	SPI_Init();
+	SS_Enable;
 	
 	UTFT display;
 	display.InitLCD(LANDSCAPE);
@@ -31,13 +51,21 @@ int main(void) {
 	
 	display.setColor(255, 250, 0);
 	
-	uint8_t pos = 0;
+	uint16_t pos = 0;
 	
 	while (1) {
-		display.fillRect(pos, 0, pos+20, 20);
-		if (pos > 200) {
+		if (isTouched()) {
+			display.setColor(255, 0, 0);
+			display.fillCircle(getX(), getY(), 10);
+			delay(100);
+		}
+		
+		display.setColor(255, 250, 0);
+		display.fillRect(pos, 0, pos+19, 19);
+		if (pos > 300) {
 			pos = 0;
-			display.clrScr();
+			display.setColor(0, 0, 0);
+			display.fillRect(0, 0, 319, 19);
 		} else {
 			pos+=20;
 		}
