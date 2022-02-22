36cf0f1bea212da917f00e347c1bfdebbcd534df
UnbuttonedHennessy <67503200+UnbuttonedHennessy@users.noreply.github.com>
2022-02-22 21:20:39 +0100

    Krizic: dodana logika

--- URS_projekt/main.cpp
+++ URS_projekt/main.cpp
@@ -24,17 +24,18 @@
 #define KRIUZIC_ICON_Y2 239
 
 // krizic ------------
-#define BOARD_X1 90
-#define BOARD_X2 231
-#define BOARD_Y1 43
-#define BOARD_Y2 184
+#define BOARD_X1 90		// left edge of the board
+#define BOARD_X2 231	// right edge of the board
+#define BOARD_Y1 43		// bottom edge of the board
+#define BOARD_Y2 184	// top edge of the board
 
-#define BORDER_X1 133
-#define BORDER_X2 182
-#define BORDER_Y1 86
-#define BORDER_Y2 135
+#define BORDER_X1 133	// left edge of left vertical line
+#define BORDER_X2 182	// left edge of right vertical line
+#define BORDER_Y1 86	// bottom edge of bottom horizontal line
+#define BORDER_Y2 135	// bottom edge of top horizontal line
 
 #define BORDER_WIDTH 6
+#define SQUARE_WIDTH 43
 // --------------------
 
 #define BACK_X1 0
@@ -78,7 +79,15 @@
 */
 uint8_t currentDisplay = 0;
 
-uint8_t tileSequence[GAME_LENGTH], highscore = 0, steps = 1, xWins = 0, oWins = 0;
+/* krizicEndFlag
+ * 0 - igra jos traje
+ * 1 - X je pobijedio
+ * 2 - O je pobijedio
+ * 3 - nerijeseno
+ */
+
+uint8_t tileSequence[GAME_LENGTH], highscore = 0, steps = 1, xWins = 0, oWins = 0, krizicEndFlag;
+char moveHistory[3][3], turn;
 UTFT display;
 
 bool isTouched() {
@@ -131,21 +140,6 @@ void simonDrawBoard() {
 	
 	display.setFont(BigFont);
 	display.printNumI(0, CENTER, SCORE_Y);
-}
-
-void krizicDrawBoard() {
-	display.clrScr();
-
-	display.setColor(255, 255, 255);
-	display.fillRect(BORDER_X1, BOARD_Y1, BORDER_X1 + BORDER_WIDTH, BOARD_Y2);
-	display.fillRect(BORDER_X2, BOARD_Y1, BORDER_X2 + BORDER_WIDTH, BOARD_Y2);
-	display.fillRect(BOARD_X1, BORDER_Y1, BOARD_X2, BORDER_Y1 + BORDER_WIDTH);
-	display.fillRect(BOARD_X1, BORDER_Y2, BOARD_X2, BORDER_Y2 + BORDER_WIDTH);
-
-	display.print("X:", HI_X, TOP_TEXT_Y);
-	display.printNumI(xWins, HI_X + 10, TOP_TEXT_Y);
-	display.print("O:", HI_X, TOP_TEXT_Y - 20);
-	display.printNumI(oWins, HI_X + 10, TOP_TEXT_Y - 20);
 }
 
 void simonBlinkTile(uint8_t tileNumber) {
@@ -262,6 +256,167 @@ void simonGameOver() {
 	display.print("Kreni", CENTER, START_TEXT_Y);
 }
 
+// draws board and initialises global variables
+void krizicInit() {
+
+	// draw board
+	display.clrScr();
+
+	display.setColor(255, 255, 255);
+	display.fillRect(BORDER_X1, BOARD_Y1, BORDER_X1 + BORDER_WIDTH, BOARD_Y2);
+	display.fillRect(BORDER_X2, BOARD_Y1, BORDER_X2 + BORDER_WIDTH, BOARD_Y2);
+	display.fillRect(BOARD_X1, BORDER_Y1, BOARD_X2, BORDER_Y1 + BORDER_WIDTH);
+	display.fillRect(BOARD_X1, BORDER_Y2, BOARD_X2, BORDER_Y2 + BORDER_WIDTH);
+
+	display.print("X:", HI_X, TOP_TEXT_Y);
+	display.printNumI(xWins, HI_X + 10, TOP_TEXT_Y);
+	display.print("O:", HI_X, TOP_TEXT_Y - 20);
+	display.printNumI(oWins, HI_X + 10, TOP_TEXT_Y - 20);
+
+	// init variables
+	uint8_t i, j;
+	turn = 'X';
+	krizicEndFlag = 0;
+
+	for (i = 0; i < 3; ++i) {
+		for (j = 0; j < 3; ++j) {
+			moveHistory[i][j] = '0';
+		}
+	}
+}
+
+// checks which square is pressed
+uint8_t krizicCheckInput() {
+	while (!isTouched());
+	uint16_t x = getX();
+	uint16_t y = getY();
+
+	/*
+	 *-1 - Povratak
+	 * 1 - Gore lijevo
+	 * 2 - Gore sredina
+	 * 3 - Gore desno
+	 * 4 - Lijevo
+	 * 5 - Sredina
+	 * 6 - Desno
+	 * 7 - Dolje lijevo
+	 * 8 - Dolje sredina
+	 * 9 - Dolje desno
+	 * 0 - Ništa od navedenog
+	 */
+
+	if ((x > BACK_X1) && (x < BACK_X2) && (y > BACK_Y1) && (y < BACK_Y2)) return -1;
+
+	else if ((x > BOARD_X1)                 && (x < BORDER_X1)  && (y > BORDER_Y2 + BORDER_WIDTH) && (y < BOARD_Y2)) return 1;
+	else if ((x > BORDER_X1 + BORDER_WIDTH) && (x < BORDER_X2)  && (y > BORDER_Y2 + BORDER_WIDTH) && (y < BOARD_Y2)) return 2;
+	else if ((x > BORDER_X2 + BORDER_WIDTH) && (x < BOARD_X2)   && (y > BORDER_Y2 + BORDER_WIDTH) && (y < BOARD_Y2)) return 3;
+
+	else if ((x > BOARD_X1)                 && (x < BORDER_X1)  && (y > BORDER_Y1 + BORDER_WIDTH) && (y < BORDER_Y2)) return 4;
+	else if ((x > BORDER_X1 + BORDER_WIDTH) && (x < BORDER_X2)  && (y > BORDER_Y1 + BORDER_WIDTH) && (y < BORDER_Y2)) return 5;
+	else if ((x > BORDER_X2 + BORDER_WIDTH) && (x < BOARD_X2)   && (y > BORDER_Y1 + BORDER_WIDTH) && (y < BORDER_Y2)) return 6;
+
+	else if ((x > BOARD_X1)                 && (x < BORDER_X1)  && (y > BOARD_Y1)                 && (y < BORDER_Y1)) return 7;
+	else if ((x > BORDER_X1 + BORDER_WIDTH) && (x < BORDER_X2)  && (y > BOARD_Y1)                 && (y < BORDER_Y1)) return 8;
+	else if ((x > BORDER_X2 + BORDER_WIDTH) && (x < BOARD_X2)   && (y > BOARD_Y1)                 && (y < BORDER_Y1)) return 9;
+
+	else return 0;
+}
+
+// verify that the pressed square was available
+// saves move (turn) to moveHistory
+// returns 1 if the chosen square is empty (value '0') or 0 if it is occupied (values 'X' and 'O')
+uint8_t krizicVerifyInput(uint8_t input) {
+	uint8_t i, j, squareNum = 0;
+
+	for (i = 0; i < 3; ++i) {
+		for (j = 0; j < 3; ++j) {
+			squareNum++;
+
+			if (squareNum == input && moveHistory[i][j] == '0') {
+				moveHistory[i][j] = turn;
+				return 1;
+
+			} else if (squareNum == input && moveHistory[i][j] != '0') return 0;
+		}
+	}
+
+	return 0;
+}
+
+// prints moves
+void krizicDrawInput(uint8_t input) { 			// add delay at the end
+}
+
+// checks if game is over
+void krizicCheckEndGame() {
+	uint8_t i, j;
+	
+	if (moveHistory[0][0] == moveHistory[0][1] && moveHistory[0][0] == moveHistory[0][2] && moveHistory[0][0] != '0' ||	// rows
+		moveHistory[1][0] == moveHistory[1][1] && moveHistory[1][0] == moveHistory[1][2] && moveHistory[1][0] != '0' ||
+		moveHistory[2][0] == moveHistory[2][1] && moveHistory[2][0] == moveHistory[2][2] && moveHistory[2][0] != '0' ||
+
+		moveHistory[0][0] == moveHistory[1][0] && moveHistory[0][0] == moveHistory[2][0] && moveHistory[0][0] != '0' ||	// columns
+		moveHistory[0][1] == moveHistory[1][1] && moveHistory[0][1] == moveHistory[2][1] && moveHistory[0][1] != '0' ||
+		moveHistory[0][2] == moveHistory[1][2] && moveHistory[0][2] == moveHistory[2][2] && moveHistory[0][2] != '0' ||
+
+		moveHistory[0][0] == moveHistory[1][1] && moveHistory[0][0] == moveHistory[2][2] && moveHistory[0][0] != '0' ||	// diagonals
+		moveHistory[0][2] == moveHistory[1][1] && moveHistory[0][2] == moveHistory[2][0] && moveHistory[0][2] != '0'){
+		
+		// only the last player to make a move can win
+		krizicEndFlag = turn == 'X' ? 1 : 2;
+	}
+
+	// checks if it's a tie (all squares will be used, but there won't be a winner)
+	for (i = 0; i < 3; ++i) {
+		for (j = 0; j < 3; ++j) {
+			if (moveHistory[i][j] == '0') return;
+		}
+	}
+	krizicEndFlag = 3;
+}
+
+// handles game over
+void krizicGameOver() {
+	switch (krizicEndFlag) {
+		case 1:		//	X won
+			xWins++;
+			// printanje
+			break;
+		case 2:		// O won
+			oWins++;
+			// printanje
+			break;
+		case 3:		// tie
+			// printanje
+			break;
+	}
+}
+
+// main game function
+void krizicGame() {
+	krizicInit();
+
+	uint8_t input;
+
+	while (!krizicEndFlag) {
+		input = krizicCheckInput();
+
+		if (input < 0) {
+			// back
+		} else if (input > 0) {
+			if (krizicVerifyInput(input)) {
+				krizicDrawInput(input);
+				krizicCheckEndGame();
+			}
+
+			turn = turn == 'X' ? 'O' : 'X';
+		}
+	}
+
+	krizicGameOver();
+}
+
+
 void openGame() {
 	uint16_t x = getX();
 	uint16_t y = getY();
@@ -286,19 +441,18 @@ int main(void) {
 	
 	while (1) {
 		if (currentDisplay == 0) {
-			while(!isTouched());
+			while (!isTouched());
 			openGame();
 		} else if (currentDisplay == 1) {
 			simonDrawBoard();
 			while (simonCheckInput() != 5);
 			simonStart();
-			while(simonVerifySequence(steps)) {
+			while (simonVerifySequence(steps)) {
 				simonNewLevel(++steps);
 			}
 			simonGameOver();
 		} else if (currentDisplay == 2) {
-			krizicDrawBoard();
+			krizicGame();
 		}
-		
 	}
 }
